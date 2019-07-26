5b3a01ad4fa7cd955d2103136cd778dd420242f2
Sreeja <sreeja_n@redbytes.in>
2019-07-26 14:53:47 +0530

    [INIT] Settings and Requirements

--- library/settings.py
+++ library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%*8-&h^fgrvdhidgo24b!$bv4^3__nbz-jgmd&qv935q8@644e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
